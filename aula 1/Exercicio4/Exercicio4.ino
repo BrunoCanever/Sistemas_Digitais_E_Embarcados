114103b8aac5be5c64fc279e2e52ff040d6e5622
Bruno Canever <Bbrnocks@gmail.com>
2025-06-03 15:13:04 -0300

    Update Exercicio4.ino

--- aula 1/Exercicio4/Exercicio4.ino
+++ aula 1/Exercicio4/Exercicio4.ino
@@ -1,9 +1,9 @@
-#define pin_led1 8
-#define pin_led2 9
-#define pin_led3 10
-#define pin_led4 11
-#define pin_botao1 2
-#define pin_botao2 3
+#define p1 8      //definindo que o pino 8 se chama p1
+#define p2 9      //definindo que o pino 9 se chama p2
+#define p3 10     //definindo que o oino 10 se chama p3
+#define p4 11     //definindo que o pino 11 se chama p4
+#define pbotao1 2  //definindo que o pino 2 se chama pbotao1
+#define pbotao2 3  //defininfo que o pino 3 se chama pbotao2
 
 void setup() {
   pinMode(pin_led1, OUTPUT);
